03f2a9728ec0aa3f50c04a8f400c5de3379f7881
gautamkumar1 <gautamraaz936@gmail.com>
2025-03-13 21:47:15 +0530

    created cotroller dummy for user details

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -36,6 +36,40 @@ const createUserDerailsMethod = async (username) => {
         return ({message:"Error in creating user details"})
     }
 }
+const createUserDerails = async (req, res) => {
+    try {
+        const {username} = req.body;
+        // const isUserExist = await User.findOne({username:username})
+        // if(isUserExist){
+        //     return res.status(400).json({message:"User already exists"})
+        // }
+        const userData = await getGithubUserInformation(username);
+        const rankData = await Rank.findOne({username:username})
+        const roastData = await Roast.findOne({username:username});
+        const userDetails = {
+            githubLink:userData.html_url,
+            name:userData.name,
+            avatar_url:userData.avatar_url,
+            username:userData.login,
+            bio:userData.bio,
+            location:userData.location,
+            followers:userData.followers,
+            following:userData.following,
+            twitter_username:userData.twitter_username,
+            location:userData.location,
+            rank:rankData.rank,
+            score:rankData.score,
+            aiNickname:rankData.nickname,
+            aiGeneratedRoast:roastData.roastMessage,
+            aiEmoji:roastData.emoji
+        }
+        const user = await User.create(userDetails);
+        return res.status(200).json({message:"User details created successfully", user})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Error in creating user details"})
+    }
+}
 const getUserDetailsByUsername = async (req, res) => {
     try {
         const {username} = req.body;
@@ -50,4 +84,4 @@ const getUserDetailsByUsername = async (req, res) => {
         
     }
 }
-export {createUserDerailsMethod,getUserDetailsByUsername}
+export {createUserDerailsMethod,getUserDetailsByUsername,createUserDerails}
--- server/src/routes/main-routes.js
+++ server/src/routes/main-routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createScoreAndRoastMsg, leaderboardLists } from "../controllers/main-controller.js";
-import { getUserDetailsByUsername } from "../controllers/user-controller.js";
+import { createUserDerails, getUserDetailsByUsername } from "../controllers/user-controller.js";
 const router = Router();
 router.post("/createScoreAndRoastMsg",createScoreAndRoastMsg);
 router.get("/getLeaderboardLists",leaderboardLists);
 router.post("/getUserDetailsByUsername",getUserDetailsByUsername)
+router.post("/createUserDerails",createUserDerails);
 export default router;
